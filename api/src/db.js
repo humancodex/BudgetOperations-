82285fd4672491c804b4fbf55b30a63a8a07348e
outsiderintrospector <juansebcarv@outlook.com>
2022-03-07 18:23:13 -0300

    update name path

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 
 const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/budgetracker`,
 	{
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -17,7 +17,7 @@ const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+
 fs.readdirSync(path.join(__dirname, "/models"))
 	.filter(
 		(file) =>
@@ -27,15 +27,15 @@ fs.readdirSync(path.join(__dirname, "/models"))
 		modelDefiners.push(require(path.join(__dirname, "/models", file)));
 	});
 
-// Injectamos la conexion (sequelize) a todos los modelos
+
 modelDefiners.forEach((model) => model(sequelize));
 
-// Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
 	entry[0][0].toUpperCase() + entry[0].slice(1),
 	entry[1],
 ]);
+
 sequelize.models = Object.fromEntries(capsEntries);
 
 const { User, Operation } = sequelize.models;
