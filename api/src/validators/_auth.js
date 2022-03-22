42a63a699ace1f4327c4542fffa14d2cf269e123
outsiderintrospector <juansebcarv@outlook.com>
2022-03-22 13:52:59 -0300

    register done

--- api/src/validators/_auth.js
+++ api/src/validators/_auth.js
@@ -1,7 +1,7 @@
 const { compare } = require("bcryptjs");
 const { check } = require("express-validator");
 
-const db = require("../db");
+const { User } = require("../db");
 
 //password
 const password = check("password")
@@ -9,40 +9,39 @@ const password = check("password")
 	.withMessage("Password has to be between 6 a 15 characters");
 
 //Email
-const email = check("email").isEmail().withMessage("Please provide a valid Email");
+const email = check("email")
+	.isEmail()
+	.withMessage("Please provide a valid Email");
 
 //check if email exist
 
 const emailExist = check("email").custom(async (value) => {
-	const { rows } = await db.query("SELECT * from users WHERE email = $1", [
-		value,
-	]);
+	let email = await User.findOne({ where: { email: value } });
 
-	if (rows.length) {
+	if (email == value) {
 		throw new Error("Email already exist ");
 	}
 });
 
 //login
-const loginFieldsCheck = check("email").custom(async (value ,{req}) => {
-	const user = await db.query("SELECT * from users WHERE email = $1", [
-		value,
-	]);
-    if(!user.rows.length){
-        throw new Error('email does not exist ')
-    }
+const loginFieldsCheck = check("email").custom(async (value, { req }) => {
+	let user = await User.findOne({ where: { email: value } });
+	// const userdb = await db.query("SELECT * from users WHERE email = $1", [value]);
 
-    const validPassword = await compare(req.body.password, user.rows[0].password)
+	if (!user) {
+		throw new Error("email does not exist ");
+	}
 
-    if(!validPassword){
-        throw new Error('Wrong password')
-    }
+	const validPassword = await compare(req.body.password, user.password);
 
-	req.user = user.rows[0]
+	if (!validPassword) {
+		throw new Error("Wrong password");
+	}
 
-})
+	req.user = user;
+});
 
 module.exports = {
 	registerValidation: [email, password, emailExist],
-    loginValidation:[loginFieldsCheck],
+	loginValidation: [loginFieldsCheck],
 };
